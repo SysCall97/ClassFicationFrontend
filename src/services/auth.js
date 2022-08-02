6590c3874ad2a5d5205504d50f7fcb7a6a9e8d75
SysCall97 <kazimashry@gmail.com>
2022-08-03 01:39:34 +0600

    new API added in the auth service

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,3 +2,4 @@ import axios_instance from "../lib/axios";
 
 export const signin = payload => axios_instance.post('/auth/signin', payload);
 export const signout = () => axios_instance.post('/auth/signout');
+export const checkToken = () => axios_instance.get('/auth/check_token');
