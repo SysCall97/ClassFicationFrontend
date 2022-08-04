25266cacbc7079106b4c3c04f6506ae6a7e103be
SysCall97 <kazimashry@gmail.com>
2022-08-04 21:14:01 +0600

    default navigation of protected routes set to home

--- src/components/common/PrivateRoute.js
+++ src/components/common/PrivateRoute.js
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ isLoggedIn, children }) => {
     if (!isLoggedIn) {
-        return <Navigate to="/signin" replace />;
+        return <Navigate to="/" replace />;
     }
     return children;
 };
