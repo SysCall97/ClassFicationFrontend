7913e8aa82061e98887e30864c5b4ff72704530e
SysCall97 <kazimashry@gmail.com>
2022-08-06 02:12:05 +0600

    added new API to get class list of an user

--- src/services/user.js
+++ src/services/user.js
@@ -2,3 +2,4 @@ import axios_instance from "../lib/axios";
 
 export const signup = payload => axios_instance.post('/user/signup', payload);
 export const update = payload => axios_instance.put('/user/update', payload);
+export const getClassList = () => axios_instance.get('/user/class_list');
