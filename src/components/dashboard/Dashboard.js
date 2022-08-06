32571710fbb17969b53666806ea9aa434350a3ad
SysCall97 <kazimashry@gmail.com>
2022-08-06 15:28:56 +0600

    classList show in the dashboard component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getClassList } from '../../services/user';
+import Card from '../common/Card';
 
 const Dashboard = () => {
     const [classList, setClassList] = useState([]);
@@ -8,8 +9,8 @@ const Dashboard = () => {
         getClassList().then(res => setClassList(res.data)).catch(err=> setClassList([]));
     }, []);
     return (
-        <div>
-            
+        <div className='classListWrapper'>
+            {classList.map(_class => <Card _class = {_class} key={_class.classCode} />)}
         </div>
     );
 };
