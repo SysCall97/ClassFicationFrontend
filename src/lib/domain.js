c0abff9e5ef95051dfb1334b552aa54e89c609bc
SysCall97 <kazimashry@gmail.com>
2022-10-01 11:27:22 +0600

    added domain file to control domain switch

---
+++ src/lib/domain.js
@@ -0,0 +1,6 @@
+export const local = "http://localhost:5000";
+export const remote = "https://class-fication-backend.herokuapp.com";
+
+export const currentBaseURL = remote;
+
+export const baseURL = `${currentBaseURL}/api/v1`;
