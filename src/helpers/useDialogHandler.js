79d3fcf22a55aa5c42b7a453ad4ae69c2dea414f
SysCall97 <kazimashry@gmail.com>
2022-10-01 11:28:17 +0600

    added maxWidth state to control the size of the widh of dialog box

--- src/helpers/useDialogHandler.js
+++ src/helpers/useDialogHandler.js
@@ -4,6 +4,7 @@ export const useDialogHandler = () => {
     const [title, setTitle] = useState("");
     const [open, setOpen] = useState(false);
     const [content, setContent] = useState("");
+    const [maxWidth, setMaxWidth] = useState("sm");
     const handleDialogClose = () => {
         setOpen(false);
         setContent("");
@@ -14,6 +15,7 @@ export const useDialogHandler = () => {
         open, setOpen,
         title, setTitle,
         content, setContent,
-        handleDialogClose
+        handleDialogClose,
+        maxWidth, setMaxWidth
     ];
 }
