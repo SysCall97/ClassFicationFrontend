447962b572871094e93ebb87fe367a4817be30b7
SysCall97 <kazimashry@gmail.com>
2022-10-01 11:27:51 +0600

    used domain in axios lib

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
+import { baseURL } from "./domain";
 
 const instance = axios.create({
-	baseURL: "http://localhost:5000/api/v1",
-	// baseURL: "https://class-fication-backend.herokuapp.com/api/v1",
+	baseURL: baseURL,
 	headers: { Authorization: "Bearer " + localStorage.tid },
 });
 
