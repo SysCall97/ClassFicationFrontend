9d6bcd82080b4e5acae9dd4a28a2c4b5fc395609
SysCall97 <kazimashry@gmail.com>
2022-11-23 02:12:35 +0600

    session service added

--- src/services/session.js
+++ src/services/session.js
@@ -1,3 +1,5 @@
 import axios_instance from "../lib/axios";
 
 export const create = ({payload, classCode}) => axios_instance.post(`session/class/${classCode}`, payload);
+export const getSessions = ({ classCode, page }) => axios_instance.get(`session/class/${classCode}?page=${page}&limit=10`);
+export const getSessionCode = ({ classCode, sessionId }) => axios_instance.get(`session/${sessionId}/class/${classCode}`);
